6c3ee90dce54a5214326c95a88b3c7aeaf1d0fef
DrozsdikAdam <drozsdik17@gmail.com>
2025-03-15 14:46:57 +0100

    generateing buttons for the popular cards

--- app.js
+++ app.js
@@ -1,31 +1,75 @@
-const fetchProducts = () => {
-    fetch("https://fakestoreapi.com/products").then(res => {
-        //console.log(res);
-        res.json().then(response => {
-            console.log(response);
-            let products = response;
-            for(let product of products)
-            {
-                product.quantity = 0;
-            }
-            var firstpic = document.getElementById("firstPop");
-            var secondpic = document.getElementById("secondPop");
-            var thirdpic = document.getElementById("thirdPop");
-            var firsttext = document.getElementById("firstText");
-            var secondtext = document.getElementById("secondText");
-            var thirdtext = document.getElementById("thirdText");
-            
-            var arraysorted = response;
-            
-            arraysorted.sort((a, b) => a.rating.rate - b.rating.rate);
-            firstpic.src = arraysorted[0].image;
-            secondpic.src = arraysorted[1].image;
-            thirdpic.src = arraysorted[2].image;
-            firsttext.innerHTML = arraysorted[0].title;
-            secondtext.innerHTML = arraysorted[1].title;
-            thirdtext.innerHTML = arraysorted[2].title;
-            //jewlery, electronics, men's clothing, woman's clothing, 
+var allproducts;
+var cart = [];
 
-        });
+const fetchProducts = () => {
+  fetch("https://fakestoreapi.com/products").then((res) => {
+    //console.log(res);
+    res.json().then((response) => {
+      console.log(response);
+
+      let products = response;
+      for (let product of products) product.quantity = 0;
+      allproducts = response;
+      let firstpic = document.getElementById("firstPop");
+      let secondpic = document.getElementById("secondPop");
+      let thirdpic = document.getElementById("thirdPop");
+      let firsttext = document.getElementById("firstText");
+      let secondtext = document.getElementById("secondText");
+      let thirdtext = document.getElementById("thirdText");
+
+      var arraysorted = response;
+
+      arraysorted.sort((a, b) => a.rating.rate - b.rating.rate);
+      firstpic.src = arraysorted[0].image;
+      secondpic.src = arraysorted[1].image;
+      thirdpic.src = arraysorted[2].image;
+      firsttext.innerHTML = arraysorted[0].title;
+      secondtext.innerHTML = arraysorted[1].title;
+      thirdtext.innerHTML = arraysorted[2].title;
+      //jewlery, electronics, men's clothing, woman's clothing,
+      PopularCards(arraysorted);
     });
-}
+  });
+};
+
+const PopularCards = (sorted) => {
+  for (let i = 1; i < 4; i++) {
+    let quantity = 0;
+    for (const item of allproducts) {
+      if (item.title == sorted[i].title) quantity = item.quantity;
+    }
+    let Cardbody = document.getElementById("PopularCard" + i);
+    if (quantity == 0) {
+
+      let div = document.createElement("div");
+      div.classList = "btn-group mx-2";
+      Cardbody.appendChild(div);
+      let button = document.createElement("button");
+      button.classList = "btn gomb text-center";
+      button.innerHTML = "Add";
+      div.appendChild(button);
+      
+    } else {
+
+      let div = document.createElement("div");
+      div.classList = "btn-group mx-2";
+      Cardbody.appendChild(div);
+      let button1 = document.createElement("button");
+      button1.classList = "btn btn-danger";
+      button1.innerHTML = "-";
+      div.appendChild(button1);
+      let input = document.createElement("input");
+      input.type = "text";
+      input.value = quantity;
+      input.id = "count";
+      input.classList = "text-center btn";
+      input.disabled = true;
+      div.appendChild(input);
+      let button2 = document.createElement("button");
+      button2.classList = "btn btn-success";
+      button2.innerHTML = "+";
+      div.appendChild(button2);
+      
+    }
+  }
+};
