5dfafce0ae3a0b30680610d135549df590171105
Kelemen Ferenc Tamás <kelemenferi85@gmail.com>
2025-04-27 11:44:38 +0200

    cart page js3

--- cartPage.js
+++ cartPage.js
@@ -29,4 +29,58 @@ const cartPage = () => {
       }
     
 
+    if (Products && Products.length > 0) {
+        Products.forEach(element => {
+            let product = document.createElement("div");
+            product.className = "cart-item d-flex containerdiv justify-content-between align-items-center flex-column flex-md-row my-3";
+            let img = document.createElement("img");
+            img.src = element.image;
+            img.className = "img-fluid rounded";
+            img.style.width = "150px";
+            img.style.height = "auto";
+            img.alt = element.title;
+            let name = document.createElement("h5");
+            name.className = "product-name";
+            name.innerText = element.title;
+            let price = document.createElement("h5");
+            price.className = "product-price";
+            price.innerText = element.price + " $";
+            let quantity = document.createElement("h5");
+            quantity.className = "product-quantity";
+            quantity.innerText = element.quantity + " db";
+            let removebtn = document.createElement("button");
+            removebtn.className = "btn btn-danger fs-4";
+            removebtn.innerHTML = "<i class='bi bi-trash'></i>";
+            removebtn.onclick = () => {
+                const productIndexInAll = allproducts.findIndex(p => p.id === element.id);
+                allproducts[productIndexInAll].quantity = 0;
+                Products.splice(Products.indexOf(element), 1);
+                localStorage.setItem("Terméklista", JSON.stringify(allproducts));
+                localStorage.setItem("Kosár", JSON.stringify(Products));
+                cartPage();
+            }
+            
+            product.appendChild(img);
+            product.appendChild(name);
+            product.appendChild(price);
+            product.appendChild(quantity);
+            product.appendChild(removebtn);
+            cartContainer.appendChild(product);
+
+            totalPrice += element.quantity * element.price;
+        });
+        totalPriceElement.innerText = totalPrice.toFixed(2) + " $"
+        
+    }
+    else{
+        cartContainer.classList.add("d-flex");
+        cartContainer.classList.add("justify-content-center");
+        cartContainer.classList.add("align-items-center");
+        cartContainer.innerHTML = "<h2 class='text-center'>Üres a kosár</h2>";
+        paybtn.disabled = true;
+        totalPriceElement.innerText = totalPrice.toFixed(2) + " $"
+    }
+
+    
+}
     }
